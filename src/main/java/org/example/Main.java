546bbc29b1fb15184a7b74a9b14eb718db8bc5e7
hwshin1 <hwshin5044@gmail.com>
2025-03-04 20:05:15 +0900

    System.out 은 표준출력

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,20 @@
 package org.example;
 
-public class Main { // 메인 클래스
-    public static void main(String[] args) { // 메인 메서드
-        //프로그램 시작점
-        System.out.println("123");
-        //프로그램 끝점
+public class Main {
+    public static void main(String[] args) {
+        // System.out은 표준출력(모니터)
+        // .은 of를 의미
+        // 프로그램은 위에서 아래로 한줄씩 실행
+        // 프로그램 시작점
+        System.out.println(1);
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("사");
+        // 프로그램 끝점
+        // 출력
+        // 1
+        // 2
+        // 3
+        // 사
     }
 }
