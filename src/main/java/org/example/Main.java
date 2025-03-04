f810a002dd9b21b69c7adf1b128e4368f2b4b6bc
hwshin1 <hwshin5044@gmail.com>
2025-03-04 20:00:51 +0900

    main 함수는 프로그램의 시작점

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main { // 메인 클래스
+    public static void main(String[] args) { // 메인 메서드
+        //프로그램 시작점
+        System.out.println("123");
+        //프로그램 끝점
+    }
+}
