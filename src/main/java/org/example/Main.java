42983b6741ed14f6019a74e4fa6cd6752b48f955
hwshin1 <hwshin5044@gmail.com>
2025-03-05 21:15:08 +0900

    else if의 쓰임

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,15 +2,45 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        // 조건문
-        int age = 18;
-        System.out.println("당신의 나이 : " + age);
+        int age = 10;
 
-        if (age >= 20) {
-            System.out.println("성인");
+        // v1
+        if (age < 10) {
+            System.out.println("아동/영유아");
         }
-        if (age < 20) {
-            System.out.println("미성년자");
+        if (age >= 10 && age < 20) {
+            System.out.println("10대");
+        }
+        if (age >= 20) {
+            System.out.println("20대 이상");
+        }
+
+        // v2
+        if (age < 10) {
+            System.out.println("아동/영유아");
+        } else if (age >= 10 && age < 20) {
+            System.out.println("10대");
+        } else if (age >= 20) {
+            System.out.println("20대 이상");
+        }
+
+        // v2-2
+        if (age < 10) {
+            System.out.println("아동/영유아");
+        } else if (age < 20) {
+            System.out.println("10대");
+        } // 여기서는 `나이가 10살 보다 크다`라는 조건을 빼도 된다. 바로 윗줄조건(age < 10)이 참으로 나와서에서 윗줄만 실행됨
+        else if (age >= 20) {
+            System.out.println("20대 이상");
+        }
+
+        // v3
+        if (age < 10) {
+            System.out.println("아동/영유아");
+        } else if (age < 20) {
+            System.out.println("10대");
+        } else { // 전체(3가지) 경우의 수 중에서 무조건 정답이 1개 존재한다면, 마지막 if문의 조건식은 지우는게 좋다.
+            System.out.println("20대 이상");
         }
     }
 }
