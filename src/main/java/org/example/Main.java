d721e2c00c97715ee84d791ee6ea7d8609d1e6f5
hwshin1 <hwshin5044@gmail.com>
2025-03-05 21:01:04 +0900

    조건문 if

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,15 +2,15 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int x = 10;
+        // 조건문
+        int age = 18;
+        System.out.println("당신의 나이 : " + age);
 
-        // 문장 + 숫자 => 숫자는 문장화 됨
-        System.out.println("x"); // x
-        System.out.println(x); // 10
-        System.out.println("x" + x); // x10
-        System.out.println("x : " + x); // x : 10
-        System.out.println("x : " + x * 10); // x : 100 곱셈 먼저
-        System.out.println("x : " + x + 10); // x :  1010
-        System.out.println("x : " + (x + 10)); // x : 20 소괄호 먼저
+        if (age >= 20) {
+            System.out.println("성인");
+        }
+        if (age < 20) {
+            System.out.println("미성년자");
+        }
     }
 }
