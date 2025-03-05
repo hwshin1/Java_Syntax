3c5211db4b932d9b209c81f97d32a094346de744
hwshin1 <hwshin5044@gmail.com>
2025-03-05 22:38:19 +0900

    String 자료형

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,71 +2,19 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int age = 8;
-        System.out.println("당신의 나이 : " + age);
+        String s = "8 * 1 = 8\n";
+        String s2 = "8 * 2 = 16\n";
+        String s3 = "8 * 3 = 24";
+        System.out.println(s + s2 + s3); // String은 문자열을 저장할 수 있는 자료형이다.
 
-        System.out.println("방법 1");
-        if (age <= 19 || age >= 60) {
-            System.out.println("할인 대상입니다.");
-        }
-        if (age > 19 && age < 60) {
-            System.out.println("할인 대상이 아닙니다.");
-        }
+        System.out.println("===========");
 
-        System.out.println("방법 2");
+        System.out.println("8 * 1 = 8");
+        System.out.println("8 * 2 = 16");
+        System.out.println("8 * 3 = 24");
 
-        if (age <= 19) {
-            System.out.println("할인 대상입니다.");
-        }
-        if (age >= 60) {
-            System.out.println("할인 대상입니다.");
-        }
-        if (age > 19 && age < 60) {
-            System.out.println("할인 대상이 아닙니다.");
-        }
+        System.out.println("===========");
 
-        System.out.println("방법 3");
-
-        if (age <= 19 || age >= 60) {
-            System.out.println("할인 대상입니다.");
-        } else if (age > 19 && age < 60) {
-            System.out.println("할인 대상이 아닙니다.");
-        }
-
-        System.out.println("방법 4");
-
-        if (age <= 19 || age >= 60) {
-            System.out.println("할인 대상입니다.");
-        } else {
-            System.out.println("할인 대상이 아닙니다.");
-        }
-
-        System.out.println("방법 5");
-
-        if (age <= 19) {
-            System.out.println("할인 대상입니다.");
-        } else if (age >= 60) {
-            System.out.println("할인 대상입니다.");
-        } else {
-            System.out.println("할인 대상이 아닙니다.");
-        }
-
-        System.out.println("방법 6");
-
-        if (age > 19 && age < 60) {
-            System.out.println("할인 대상이 아닙니다.");
-        } else {
-            System.out.println("할인 대상입니다.");
-        }
-
-        System.out.println("방법 7");
-
-        if (age <= 19 || age >= 60) {
-            System.out.println("할인 대상입니다.");
-        } else if (age > 19) {
-            if (age < 60) {
-                System.out.println("할인 대상이 아닙니다.");
-            }
-        }
+        System.out.println("8 * 1 = 8\n8 * 2 = 16\n8 * 3 = 24");
     }
 }
