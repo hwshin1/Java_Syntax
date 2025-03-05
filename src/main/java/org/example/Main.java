2164a1cfa280ed21dc7085b84531418e8fa9bfac
hwshin1 <hwshin5044@gmail.com>
2025-03-05 21:25:35 +0900

    증감연산 추가

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,5 +14,15 @@ public class Main {
 
         i++; // i의 값을 1 증가시킨다 <= 3개중에서 가장 많이 쓰는 증감연산
         System.out.println(i);
+
+        i += 2; // i의 값을 2 증가시킨다
+        System.out.println(i);
+
+        System.out.println("===============");
+
+        int j = 100;
+
+        j -= 10; // j의 값을 10씩 감소시킨다
+        System.out.println(j);
     }
 }
