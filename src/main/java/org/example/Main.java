0295137bd48ddc3f74b4a56809a2ce6e2b599722
hwshin1 <hwshin5044@gmail.com>
2025-03-06 22:29:38 +0900

    반복문

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 // 문제 : 2부터 9까지의 숫자를 사용하지 않고 구구단 출력을 완성 해주세요.
+// * 100까지 출력
 public class Main {
     public static void main(String[] args) {
         int dan = 5;
@@ -22,7 +23,15 @@ public class Main {
         i = i + 1;
         System.out.println(dan + " * " + i + " = " + dan * i);
         i = i + 1;
-        System.out.println(dan + " * " + i + " = " + dan * i);
-        // 코드는 위에서 아래로 읽기 때문에 출력문 다음에 i를 1씩 증가하는 문장을 넣으면 i는 1,2,3,4,5 이런식으로 변화한다.
+        System.out.println(dan + " * " + i + " = " + dan * i); // 이걸 100개를 추가를 하면 어디까지 출력이 되었는지 확인이 안되고 코드가 길어짐
+
+        System.out.println("===========================");
+        // while 문
+        int j = 1;
+
+        while (j <= 100) {
+            System.out.println(dan + " * " + j + " = " + dan * j);
+            j++;
+        } // 반복문을 쓰면 코드를 줄일수 있다.
     }
 }
