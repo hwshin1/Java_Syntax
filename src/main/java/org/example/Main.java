dc610a986589be64d8c03a22ed1e318193253189
hwshin1 <hwshin5044@gmail.com>
2025-03-06 22:22:40 +0900

    2~9 숫자를 사용하지 않고 구구단 출력

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,28 @@
 package org.example;
 
+// 문제 : 2부터 9까지의 숫자를 사용하지 않고 구구단 출력을 완성 해주세요.
 public class Main {
     public static void main(String[] args) {
-        String s = "8 * 1 = 8\n";
-        String s2 = "8 * 2 = 16\n";
-        String s3 = "8 * 3 = 24";
-        System.out.println(s + s2 + s3); // String은 문자열을 저장할 수 있는 자료형이다.
+        int dan = 5;
 
-        System.out.println("===========");
-
-        System.out.println("8 * 1 = 8");
-        System.out.println("8 * 2 = 16");
-        System.out.println("8 * 3 = 24");
-
-        System.out.println("===========");
-
-        System.out.println("8 * 1 = 8\n8 * 2 = 16\n8 * 3 = 24");
+        int i = 1;
+        System.out.println(dan + " * " + i + " = " + dan * i);
+        i = i + 1; // i를 1씩 증가한다
+        System.out.println(dan + " * " + i + " = " + dan * i);
+        i = i + 1;
+        System.out.println(dan + " * " + i + " = " + dan * i);
+        i = i + 1;
+        System.out.println(dan + " * " + i + " = " + dan * i);
+        i = i + 1;
+        System.out.println(dan + " * " + i + " = " + dan * i);
+        i = i + 1;
+        System.out.println(dan + " * " + i + " = " + dan * i);
+        i = i + 1;
+        System.out.println(dan + " * " + i + " = " + dan * i);
+        i = i + 1;
+        System.out.println(dan + " * " + i + " = " + dan * i);
+        i = i + 1;
+        System.out.println(dan + " * " + i + " = " + dan * i);
+        // 코드는 위에서 아래로 읽기 때문에 출력문 다음에 i를 1씩 증가하는 문장을 넣으면 i는 1,2,3,4,5 이런식으로 변화한다.
     }
 }
