4098c5ac47f90f6877f5d296590f11ba50ae212b
hwshin1 <hwshin5044@gmail.com>
2025-03-08 22:15:15 +0900

    부동소수점 연산

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,18 +2,17 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        //후위연산
-        int a = 1;
-        int i = a++; // a가 i에 대입을 먼저 한후 a에 1을 더한다
-
-        System.out.println(a); // 2
-        System.out.println(i); // 2가 아니라 1이 나옴
-
-        //전위연산
-        int b = 1;
-        int j = ++b; // b에 1을 먼저 더한후 j에 대입을 한다
-
-        System.out.println(b); // 2
-        System.out.println(j); // 2 가 나옴
+        double a = 2.1;
+        if ((a + a + a ) == 6.3) {
+            System.out.println("같음");
+        } else {
+            System.out.println("다름"); // 이게 실행된다.
+        }
+        // float와 double 타입 부동소수점 연산
+        // 다름이 나오는 이유 실수를 계산 할 때에는 정확도가 부정확 할 수 있다
+        /* 부동소수점 연산
+        넓은 범위의 수를 빠르게 정밀한 '근사치'로 계산하도록 되어있다.
+        따라서 정확한 결과가 필요할 때는 사용하면 안된다.
+        */
     }
 }
