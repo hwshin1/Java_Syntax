363f512234e35c8f8c0cb9c3a140cc72dec50ec3
hwshin1 <hwshin5044@gmail.com>
2025-03-10 23:40:23 +0900

    기본형 타입들 연습

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,25 +2,29 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        // 기본형 타입 변수
         //논리
-        boolean b = true;
+        boolean b = false;
 
         //문자
-        char c = 'a';
+        char c = 'b';
 
         //정수
-        byte bt = 1;
-        short s = 2;
-        int i = 3;
-        long l = 4L;
+        byte bt = 4;
+        short s = 8;
+        int i = 10;
+        long l = 5L;
 
         // 실수
-        float f = 5.5f;
-        double d = 6.5;
+        float f = 6.21f;
+        double d = 6.5415;
 
-        // 기본형 변수 이외의 타입들은 참조형 타입의 변수라고 한다
-        // 참조형 변수는 실체가 없다 실체는 외부에 저장.
-        // 참조형 변수에는 실체를 조종 할 수 있는 리모컨이 저장된다.
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(bt);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
     }
 }
