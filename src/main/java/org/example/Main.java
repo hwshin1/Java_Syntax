3cd96411fc484bec9892afd525d191fa6c9b21c6
hwshin1 <hwshin5044@gmail.com>
2025-03-13 22:30:25 +0900

    배열의 합/평균

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,21 +2,27 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = new int[10];
+        int[] arr = new int[7];
 
-        /*
-        arr[0] = 1;
-        arr[1] = 2;
-        arr[2] = 3;
-        arr[3] = 4;
-        arr[4] = 5;
-        ... int[] 배열의 갯수가 길어질수록 값을 넣는게 많아진다
-        */
+        // arr[0] = 10;
+        // arr[1] = 20;
+        // arr[2] = 30;
 
-        // 반복문을 사용해서 arr[]에 값을 넣기
-        for (int i = 0; i < arr.length; i++) { // i = 0 부터 시작하는 이유 배열은 arr[0]부터 시작함
-            arr[i] = i + 1; // 값이 1부터 들어간다
-            System.out.println("arr[" + i + "] : " + arr[i]);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (i + 1) * 10; // 10, 20, 30씩 값을 넣는다.
         }
+        // arr[0] 부터 값을 넣는다
+
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        // 배열의 합을 구한다.
+
+        int average = sum / arr.length;
+        // 배열의 합과 배열의 길이를 나누면 평균이 된다.
+
+        System.out.printf("총합 : %d\n", sum);
+        System.out.printf("평균 : %d\n", average);
     }
 }
