d550962a669c6f5798137045a942aee661ac5986
hwshin1 <hwshin5044@gmail.com>
2025-03-14 22:10:30 +0900

    정수 100개 컨트롤 / 100개의 합

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,27 +2,21 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = new int[7];
-
-        // arr[0] = 10;
-        // arr[1] = 20;
-        // arr[2] = 30;
+        // 정수 100개 컨트롤
+        int[] arr = new int[100];
 
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = (i + 1) * 10; // 10, 20, 30씩 값을 넣는다.
+            arr[i] = i + 1;
+            System.out.println(arr[i]);
         }
-        // arr[0] 부터 값을 넣는다
 
+        System.out.println("============");
+        // 정수 100개의 합.
         int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            sum += arr[i];
+        for(int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i]; // sum에 배열의 값들을 다 더한것을 넣는다.
         }
-        // 배열의 합을 구한다.
 
-        int average = sum / arr.length;
-        // 배열의 합과 배열의 길이를 나누면 평균이 된다.
-
-        System.out.printf("총합 : %d\n", sum);
-        System.out.printf("평균 : %d\n", average);
+        System.out.println(sum);
     }
 }
