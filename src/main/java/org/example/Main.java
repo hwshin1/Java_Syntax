3cde04f1b536c5ec505ee690fc98aa683abdd868
hwshin1 <hwshin5044@gmail.com>
2025-03-15 21:57:07 +0900

    2차원 배열

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,21 +2,25 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        // 정수 100개 컨트롤
-        int[] arr = new int[100];
+        int[] arr = new int[6];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        arr[3] = 4;
+        arr[4] = 5;
+        arr[5] = 6;
 
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = i + 1;
-            System.out.println(arr[i]);
-        }
+        System.out.println(arr[4]);
 
-        System.out.println("============");
-        // 정수 100개의 합.
-        int sum = 0;
-        for(int i = 0; i < arr.length; i++) {
-            sum = sum + arr[i]; // sum에 배열의 값들을 다 더한것을 넣는다.
-        }
+        // 2차원 배열
+        int[][] arr2 = new int[2][3];
+        arr2[0][0] = 1;
+        arr2[0][1] = 2;
+        arr2[0][2] = 3;
+        arr2[1][0] = 4;
+        arr2[1][1] = 5;
+        arr2[1][2] = 6;
 
-        System.out.println(sum);
+        System.out.println(arr2[1][1]);
     }
 }
