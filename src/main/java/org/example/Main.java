0102b3db801eb9e72e6963d8e4f42a03426f6cdb
hwshin1 <hwshin5044@gmail.com>
2025-03-16 18:30:24 +0900

    피보나치 수열

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,25 +2,15 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = new int[6];
-        arr[0] = 1;
-        arr[1] = 2;
-        arr[2] = 3;
-        arr[3] = 4;
-        arr[4] = 5;
-        arr[5] = 6;
+        //피보나치 수열
+        int n = 10, firstTerm = 0, secondTerm = 1;
 
-        System.out.println(arr[4]);
+        for (int i = 1; i <= n; ++i) {
+            System.out.println(firstTerm + ", ");
 
-        // 2차원 배열
-        int[][] arr2 = new int[2][3];
-        arr2[0][0] = 1;
-        arr2[0][1] = 2;
-        arr2[0][2] = 3;
-        arr2[1][0] = 4;
-        arr2[1][1] = 5;
-        arr2[1][2] = 6;
-
-        System.out.println(arr2[1][1]);
+            int nextTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+        }
     }
 }
