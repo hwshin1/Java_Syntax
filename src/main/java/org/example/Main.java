125483002339ab8a5cb1efb3c20c96af747cbf21
hwshin1 <hwshin5044@gmail.com>
2025-03-18 20:06:46 +0900

    자동차 설계도

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,24 +1,13 @@
 package org.example;
 
+// 문제 : 자동차 설계도를 만들어주세요.
 public class Main {
     public static void main(String[] args) {
-        GameCharater person1 = new GameCharater();
-        person1.num = 1;
-        person1.age = 22;
-        person1.height = 160;
 
-        GameCharater person2 = new GameCharater();
-        person2.num = 2;
-        person2.age = 30;
-        person2.height = 180;
-
-        // 훨씬 데이터를 다루기가 쉽다.
     }
 }
 
-// class로 직접 객체를 만들기
-class GameCharater {
-    int num;
-    int age;
-    int height;
+// class를 설계도라고 표현하기도 한다.
+class 자동차 {
+    // 설계도 끝!
 }
