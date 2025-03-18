964a465e440eed5a98e936b4144aefb48ca971df
hwshin1 <hwshin5044@gmail.com>
2025-03-18 19:51:05 +0900

    class로 객체 만들기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,31 +2,23 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("== 객체 없는 Ver ==");
-        // 사람1 => 학생
-        int person1_num = 1; // 사람1의 번호
-        int person1_age = 22; // 사람1의 나이
-        int person1_height = 160; // 사람 1의 키
+        GameCharater person1 = new GameCharater();
+        person1.num = 1;
+        person1.age = 22;
+        person1.height = 160;
 
-        // 사람2 => 학생
-        int person2_num = 2; // 사람2의 번호
-        int person2_age = 20; // 사람2의 나이
-        int person2_height = 175; // 사람2의 키
+        GameCharater person2 = new GameCharater();
+        person2.num = 2;
+        person2.age = 30;
+        person2.height = 180;
 
-        // 객체 없는 버전 특징
-        // 변수를 이름이 있는 변수로 변경하면 어떤 내용인지는 알 수 있다.
-
-        System.out.println("== 객체 사용 Ver ==");
-        int[] person1 = new int[3];
-        person1[0] = 1; // 사람1 번호
-        person1[1] = 22; // 사람1 나이
-        person1[2] = 160; // 사람1 키
-
-        int[] person2 = new int[3];
-        person2[0] = 2; // 사람 2의 번호, 인스턴스 변수
-        person2[1] = 30; // 사람 2의 나이, 인스턴스 변수
-        person2[2] = 180; // 사람 2의 키, 인스턴스 변수
-
-        // 이건 주석을 달아야지만 어떤 코드인지 알 수 있다.
+        // 훨씬 데이터를 다루기가 쉽다.
     }
+}
+
+// class로 직접 객체를 만들기
+class GameCharater {
+    int num;
+    int age;
+    int height;
 }
