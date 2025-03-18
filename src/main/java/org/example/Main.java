f8cd42dfb9ac760915322332f7a05b0fb3be53d3
hwshin1 <hwshin5044@gmail.com>
2025-03-18 20:14:44 +0900

    자동차 객체마다 서로 다른 최고속력를 가지도록 하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,18 @@
 package org.example;
 
-// 문제 : 자동차 객체를 만들고 변수에 담아주세요.
+// 문제 : 자동차 객체마다 서로 다른 최고속력를 가지도록 해주세요.
 public class Main {
     public static void main(String[] args) {
-        자동차 a자동차; // 변수선언
+        자동차 a자동차1 = new 자동차();
+        a자동차1.최고속력 = 180;
+        System.out.println("a자동차1 최고속력: " + a자동차1.최고속력);
 
-        a자동차 = new 자동차(); // 객체 생성
+        자동차 a자동차2 = new 자동차();
+        a자동차2.최고속력 = 210;
+        System.out.println("a자동차2 최고속력: " + a자동차2.최고속력);
     }
 }
 
 class 자동차 {
-
+    int 최고속력;
 }
