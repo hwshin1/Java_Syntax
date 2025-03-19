ea6d657f43b99ef6c0b53fdb63e8ee592231197b
hwshin1 <hwshin5044@gmail.com>
2025-03-19 23:51:21 +0900

    1개의 자동차가 3번 달리게 하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,25 @@
 package org.example;
 
-// 문제 : 자동차 객체마다 서로 다른 최고속력를 가지도록 해주세요.
+// 문제 : 1개의 자동차가 3번 달리게 해주세요.
 public class Main {
     public static void main(String[] args) {
         자동차 a자동차1 = new 자동차();
         a자동차1.최고속력 = 180;
-        System.out.println("a자동차1 최고속력: " + a자동차1.최고속력);
 
-        자동차 a자동차2 = new 자동차();
-        a자동차2.최고속력 = 210;
-        System.out.println("a자동차2 최고속력: " + a자동차2.최고속력);
+        a자동차1.달리다();
+        a자동차1.달리다();
+        a자동차1.달리다();
+
+        // 자동차 a자동차2 = new 자동차();
+        // a자동차2.최고속력 = 210;
+        // System.out.println("a자동차2 최고속력: " + a자동차2.최고속력);
     }
 }
 
 class 자동차 {
     int 최고속력;
+
+    void 달리다() {
+        System.out.println("자동차가 달립니다.");
+    }
 }
