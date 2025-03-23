6d4730b519c12ac9755ecba6c8d471ae35cb6078
hwshin1 <hwshin5044@gmail.com>
2025-03-23 21:43:50 +0900

    피라미드형 별 찍기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,13 +2,16 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int rows = 3;
+        int rows = 5;
 
         for (int i = 1; i <= rows; i++){
-            for (int j = 1; j <= i; j++) {
-                System.out.print("* "); // println이 아닌 print
+            for (int j = i; j < rows; j++) {
+                System.out.print(" ");
             }
-            System.out.println(); // 줄바꿈
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
