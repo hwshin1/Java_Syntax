c843e81591ef81a30cb06ace888cadef7356bbb4
hwshin1 <hwshin5044@gmail.com>
2025-03-27 20:23:15 +0900

    숫자로 역삼각형 출력

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,20 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int n = 10;
-        int firstTerm = 0;
-        int secondTerm = 1;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
-        for (int i = 1; i <= n; ++i) {
-            System.out.println(firstTerm + (", "));
-
-            int nextTerm = firstTerm + secondTerm;
-            firstTerm = secondTerm;
-            secondTerm = nextTerm;
+        for (int i = n; i >= 1; i--) {
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = i; k >= 1; k--) {
+                System.out.print(" " + k);
+            }
+            System.out.println();
         }
     }
 }
