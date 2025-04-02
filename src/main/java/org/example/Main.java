fbef09c46643c2646032d0ac57a2e836c0ac33f2
hwshin1 <hwshin5044@gmail.com>
2025-04-02 20:34:55 +0900

    class로 객체 만들기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,21 @@
 package org.example;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        GameCharacter gameCharacter1 = new GameCharacter();
 
-        for (int i = n; i >= 1; i--) {
-            for (int j = n; j > i; j--) {
-                System.out.print(" ");
-            }
-            for (int k = i; k >= 1; k--) {
-                System.out.print(" " + k);
-            }
-            System.out.println();
-        }
+        int 게임번호 = gameCharacter1.no = 1001;
+        int 나이 = gameCharacter1.age = 20;
+        String 직업 = gameCharacter1.job = "전사";
+
+        System.out.println("Game Number : " + 게임번호);
+        System.out.println("age : " + 나이);
+        System.out.println("Game Job : " + 직업);
     }
+}
+
+class GameCharacter {
+    int no;
+    int age;
+    String job;
 }
