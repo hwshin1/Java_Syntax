166e4de3c0156ad30661a15c666fa5ae0b257256
hwshin1 <hwshin5044@gmail.com>
2025-04-06 19:58:05 +0900

    문제 - 객체화 없이 설계도에 있는 능력을 바로 사용해주세요.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,22 +1,15 @@
 package org.example;
 
+// 문제 : 아래 코드가 실행되도록 자동차 클래스를 만들어주세요.
 public class Main {
     public static void main(String[] args) {
-//        GameCharacter gameCharacter = new GameCharacter(); // static으로 바로 GameCharacter로 사용 가능
-
-        int 게임번호 = GameCharacter.no = 1001;
-        int 나이 = GameCharacter.age = 20;
-        String 직업 = GameCharacter.job = "전사";
-
-        System.out.println("Game Number : " + 게임번호);
-        System.out.println("age : " + 나이);
-        System.out.println("Game Job : " + 직업);
+        자동차.달리다();
     }
 }
 
-class GameCharacter {
-    // 스태틱 변수로 만들면 객체를 가지지 않고 Main 메서드에서 사용이 가능
-    static int no;
-    static int age;
-    static String job;
+class 자동차 {
+    // static 변수로 만들면 객체를 가지지 않고 Main 메서드에서 사용이 가능
+    static void 달리다() {
+        System.out.println("자동차가 달립니다.");
+    }
 }
