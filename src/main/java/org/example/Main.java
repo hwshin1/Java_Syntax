9c1d06948cb0fa85fe03a698dfa9fd9c43fea6b1
hwshin1 <hwshin5044@gmail.com>
2025-04-07 23:55:36 +0900

    계산기 static

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,23 @@
 package org.example;
 
-// 문제 : 아래 코드가 실행되도록 자동차 클래스를 만들어주세요.
 public class Main {
     public static void main(String[] args) {
-        자동차.달리다();
+        계산기.합(5, 3);
+        계산기.차(7, 4);
+        계산기.곱(2, 3);
     }
 }
 
-class 자동차 {
-    // static 변수로 만들면 객체를 가지지 않고 Main 메서드에서 사용이 가능
-    static void 달리다() {
-        System.out.println("자동차가 달립니다.");
+class 계산기 {
+    static void 합(int a, int b) {
+        System.out.println(a + b);
+    }
+
+    public static void 차(int a, int b) {
+        System.out.println(a - b);
+    }
+
+    public static void 곱(int a, int b) {
+        System.out.println(a * b);
     }
 }
