2f26f050aa54734e2ed9f158827fc65369be7c18
hwshin1 <hwshin5044@gmail.com>
2025-04-10 23:23:02 +0900

    n부터 m까지의 수의 합을 반환하는 함수

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,8 +15,11 @@ public class Main {
 class Math {
     // static을 써서 객체 생성을 안하고 바로 가져 올수 있다.
     static int nToMSum(int n, int m) { // 매개변수를 넣어 값 가져오기
-        int sum = n + m;
+        int sum = 0;
+        for (; n <= m; n++) {
+            sum = sum + n;
+        }
 
-        return sum; // 리턴으로 반환
+        return sum;
     }
 }
