39da4e1082942fb239b01129b9ee169aaafe0da5
hwshin1 <hwshin5044@gmail.com>
2025-04-11 20:23:35 +0900

    레퍼런스 변수에 값 할당과정 설명

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,30 @@
 package org.example;
 
+// 레퍼런스 변수에 값 할당 과정 설명
 public class Main {
     public static void main(String[] args) {
-        int 결과1 = Math.nToMSum(2, 3);
-        System.out.println("결과1 : " + 결과1);
-        // 출력 : 결과1 : 5
+        // 오직 Car 이라는 리모콘이 담긴 것만 저장 할 수 있는 변수 acar을 만든다.
+        // acar에는 자동차 클래스로 만든 리모콘이 들어가게 된다.
+        Car acar;
 
-        int 결과2 = Math.nToMSum(5, 10);
-        System.out.println("결과2 : " + 결과2);
-        // 출력 : 결과2 : 45
+        // Car 설계도(클래스)로 Car 객체를 변수 외부에 만든다.
+        // Car 객체는 자신을 조종할 수 있는 리모콘(this)를 가지고 태어난다.
+        // 변수 acar에 Car 객체를 조종 할 수 있는 리모콘을 복사해서 반환한다.
+        acar = new Car();
+
+        // 변수 acar 안에 있는 Car 설계도를 가진 리모콘에 run 버튼을 누른다.
+        // 해당 신호가 리모콘과 연결되어 있는 Car 객체에 전달돼 run 메소드를 실행한다.
+        acar.run();
     }
 }
 
-class Math {
-    // static을 써서 객체 생성을 안하고 바로 가져 올수 있다.
-    static int nToMSum(int n, int m) { // 매개변수를 넣어 값 가져오기
-        int sum = 0;
-        for (; n <= m; n++) {
-            sum = sum + n;
-        }
-
-        return sum;
+// Car 설계도
+class Car {
+    // run 메소드 기능
+    // run 기능 생성
+    // 리모콘 버튼
+    void run() {
+        // run 기능 '자동차가 달립니다.' 를 출력해준다.
+        System.out.println("자동차가 달립니다.");
     }
 }
