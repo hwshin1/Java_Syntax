de464b842501ca76a06e5737cc23bf3f9d817b69
hwshin1 <hwshin5044@gmail.com>
2025-04-11 20:36:16 +0900

    클래스 5개 만들고 각각 객체화 해서 호출

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,30 +1,58 @@
 package org.example;
 
-// 레퍼런스 변수에 값 할당 과정 설명
 public class Main {
     public static void main(String[] args) {
-        // 오직 Car 이라는 리모콘이 담긴 것만 저장 할 수 있는 변수 acar을 만든다.
-        // acar에는 자동차 클래스로 만든 리모콘이 들어가게 된다.
-        Car acar;
+        계좌 b = new 계좌();
+        b.bankNumber(1);
+        b.bankNumber(2);
+        b.bankNumber(3);
 
-        // Car 설계도(클래스)로 Car 객체를 변수 외부에 만든다.
-        // Car 객체는 자신을 조종할 수 있는 리모콘(this)를 가지고 태어난다.
-        // 변수 acar에 Car 객체를 조종 할 수 있는 리모콘을 복사해서 반환한다.
-        acar = new Car();
+        입출금 m = new 입출금();
+        m.addMoney(2000);
+        m.outMoney(500);
 
-        // 변수 acar 안에 있는 Car 설계도를 가진 리모콘에 run 버튼을 누른다.
-        // 해당 신호가 리모콘과 연결되어 있는 Car 객체에 전달돼 run 메소드를 실행한다.
-        acar.run();
+        에어컨 ac = new 에어컨();
+        ac.번호(3001);
+
+        핸드폰 hp = new 핸드폰();
+        hp.가격(60000, 50000);
+
+        자동차 ca = new 자동차();
+        ca.최고속도(280);
     }
 }
 
-// Car 설계도
-class Car {
-    // run 메소드 기능
-    // run 기능 생성
-    // 리모콘 버튼
-    void run() {
-        // run 기능 '자동차가 달립니다.' 를 출력해준다.
-        System.out.println("자동차가 달립니다.");
+class 계좌 {
+    void bankNumber(int i) {
+        System.out.println("계좌번호 : " + i);
+    }
+}
+
+class 입출금 {
+    void addMoney(int m) {
+        System.out.println("입금 : " +  m + "원");
+    }
+
+    void outMoney(int m) {
+        System.out.println("출금 : " + m + "원");
+    }
+}
+
+class 에어컨 {
+    void 번호(int i) {
+        System.out.println("제품 번호 : " + i);
+    }
+}
+
+class 핸드폰 {
+    void 가격(int m, int n) {
+        int result = m + n;
+        System.out.println("가격 : " + result + "원");
+    }
+}
+
+class 자동차 {
+    void 최고속도(int max) {
+        System.out.println("최고 속력 : " + max + "km/h");
     }
 }
