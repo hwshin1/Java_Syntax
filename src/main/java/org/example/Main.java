9528c829ec02cd655aa8180a70a0d7967c21e4ac
hwshin1 <hwshin5044@gmail.com>
2025-04-14 23:10:48 +0900

    소수의 갯수 구하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,14 +2,53 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int num = 32;
+        int rs = one_to_n_prime_numbers_count(10);
+        System.out.println("rs : " + rs);
+        // rs : 4
+        rs = one_to_n_prime_numbers_count(13);
+        System.out.println("rs : " + rs);
+        // rs :  6
 
-        for (int i = 1; i <= num; i++) {
-            if (num == 1) {
-                System.out.println(num);
-            } else if (num % i == 0) {
+        rs = Math.n_to_m_prime_numbers_sum(5, 11);
+        System.out.println("5부터 11까지 소수 합 : " + rs);
+    }
+
+    static int one_to_n_prime_numbers_count(int n) {
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (Math.isPrimeNumber(i)) {
                 System.out.println(i);
+                count++;
             }
         }
+        return count;
     }
+}
+
+class Math {
+    static int n_to_m_prime_numbers_sum(int n, int m) {
+        int sum = 0;
+        for (int i = n; i <= m; i++) {
+            if (Math.isPrimeNumber(i)) {
+                System.out.println(i);
+                sum += i;
+            }
+        }
+        return sum;
+    }
+
+    static boolean isPrimeNumber(int num) {
+        if (num == 1) {
+            return false;
+        }
+
+        int sum = 0;
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) { // i 는 num 의 약수 -> 약수 찾았다!
+                sum += i;
+            }
+        }
+
+        return sum == num + 1;
+    } // 소수 판별기
 }
