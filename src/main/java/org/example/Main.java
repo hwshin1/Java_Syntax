5fd8e7fb3313be3b132c2063616a2bdc6f618deb
hwshin1 <hwshin5044@gmail.com>
2025-04-20 14:50:12 +0900

    무기 클래스로 만든 리모콘으로 칼 객체를 조종

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,23 +2,16 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        // `a칼` 에는 오직 칼 리모콘만 들어간다.
-        칼 a칼 = new 칼();
-
-        a칼 = new 활();
-        // 에러가 뜬다.
-        // 이유는 활 객체는 칼 객체에 상속 받고 있다.
-        // 근데 상속을 해주는 칼에 쏘기() 메서드가 없어 인식을 못한다.
-        a칼.쏘기();
+        무기 a무기 = new 칼();
+        a무기.공격();
     }
 }
 
-class 칼 {
-    // 활이 칼을 상속 받을 수 있게 칼 객체에 적어준다.
-    void 쏘기() {
-        System.out.println("활을 쏩니다.");
+class 무기 {
+    void 공격() {
+        System.out.println("공격하기 위해 무기를 들었다.");
     }
 }
 
-class 활 extends 칼 {
+class 칼 extends 무기 {
 }
