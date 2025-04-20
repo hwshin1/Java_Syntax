a5b994fc527b67fdba43f64b8f0c10c8d3dd1abf
hwshin1 <hwshin5044@gmail.com>
2025-04-20 14:57:58 +0900

    객체는 인스턴스 변수를 통해서 상태를 기억

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,16 +2,27 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        무기 a무기 = new 칼();
-        a무기.공격();
+        전사 a전사 = new 전사();
+        a전사.공격("브라이언", "칼");
+        a전사.재공격();
+        a전사.공격("필립", "방패");
+        a전사.공격("마크", "지팡이");
+        a전사.재공격();
+        a전사.재공격();
     }
 }
 
-class 무기 {
-    void 공격() {
-        System.out.println("공격하기 위해 무기를 들었다.");
-    }
-}
+class 전사 {
+    String lastName;
+    String waponName;
+    void 공격(String name, String wapon) {
+        System.out.println(name + "(이)가 " + wapon + "(으)로 공격합니다.");
 
-class 칼 extends 무기 {
+        lastName = name;
+        waponName = wapon;
+    }
+
+    void 재공격() {
+        System.out.println(lastName + "(이)가 " + waponName + "(으)로 재공격 합니다.");
+    }
 }
